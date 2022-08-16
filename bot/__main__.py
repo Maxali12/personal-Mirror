286ac60fc6c70dffc6d5a4b7589ff6b3bd5a3e54
Maxali12 <105766185+Maxali12@users.noreply.github.com>
2022-08-17 02:34:35 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,10 +64,10 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("╚»★«╝ Mirror Group ╚»★«╝", "https://t.me/+XvFNNP762rcyOTll")
+    buttons.buildbutton("Update", "https://t.me/mirrorcloudbd")
+    buttons.buildbutton("Mirror Group 2", "https://t.me/mirrorcloudbd1")
+    buttons.buildbutton("Owner", "https://t.me/bdbots")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
